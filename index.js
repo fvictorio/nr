ab5b9d067987a76a1f463c9dc5c68314d08b54ec
Franco Victorio <victorio.franco@gmail.com>
2018-09-19 12:19:10 -0300

    Use spawn instead of shelljs

--- index.js
+++ index.js
@@ -2,18 +2,27 @@
 
 const inquirer = require('inquirer')
 const fs = require('fs')
-const shell = require('shelljs')
+const { spawn } = require('child_process')
 
-const scripts = Object.keys(JSON.parse(fs.readFileSync('package.json')).scripts)
+if (process.argv.length > 2) {
+  runScript(process.argv[2])
+} else {
+  const scripts = Object.keys(JSON.parse(fs.readFileSync('package.json')).scripts)
 
-inquirer.prompt([
-  {
-    name: 'script',
-    type: 'list',
-    message: 'Which script do you want to run?',
-    choices: scripts
-  }
-])
-  .then(answers => {
-    shell.exec(`npm run ${answers.script}`, {stdio: 'inherit'})
-  })
+  inquirer
+    .prompt([
+      {
+        name: 'script',
+        type: 'list',
+        message: 'Which script do you want to run?',
+        choices: scripts
+      }
+    ])
+    .then(answers => {
+      runScript(answers.script)
+    })
+}
+
+function runScript(script) {
+  spawn(`npm`, ['run', script], { stdio: 'inherit' })
+}
