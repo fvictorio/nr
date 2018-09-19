73d5583e0942b3c6ae2c704ffc4a1b7174dafd7f
gillchristian <gillchristiang@gmail.com>
2018-09-19 22:16:58 +0200

    List scripts when prompting what to run

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
 #!/usr/bin/env node
 
-const inquirer = require('inquirer')
 const fs = require('fs')
 const { spawn } = require('child_process')
 
+const inquirer = require('inquirer')
+const chalk = require('chalk')
+
 if (process.argv.length > 2) {
-  runScript(process.argv[2])
+  runScript(...process.argv.slice(2))
 } else {
-  const scripts = Object.keys(JSON.parse(fs.readFileSync('package.json')).scripts)
+  main()
+}
+
+function main() {
+  const pkg = fs.readFileSync('package.json')
+  const scripts = JSON.parse(pkg).scripts || {}
+  const names = Object.keys(scripts)
+
+  if (!names.length) {
+    console.log('No scripts in package.json')
+
+    return
+  }
+
+  logScripts(scripts)
 
   inquirer
     .prompt([
@@ -15,14 +31,33 @@ if (process.argv.length > 2) {
         name: 'script',
         type: 'list',
         message: 'Which script do you want to run?',
-        choices: scripts
-      }
+        choices: names,
+      },
     ])
     .then(answers => {
       runScript(answers.script)
     })
 }
 
-function runScript(script) {
-  spawn(`npm`, ['run', script], { stdio: 'inherit' })
+function runScript(script, ...args) {
+  spawn(`npm`, ['run', script, ...args], { stdio: 'inherit' })
+}
+
+function logScripts(scripts = {}) {
+  const entries = Object.entries(scripts)
+
+  const longest = entries.reduce(
+    (acc, [name]) => (name.length > acc ? name.length : acc),
+    0,
+  )
+
+  const table = entries.reduce(
+    (acc, [n, c]) => chalk`${acc}{bold ${n.padStart(longest, ' ')}} - ${c}\n`,
+    '',
+  )
+
+  console.log('-----'.padStart(longest + 4, ' '))
+  console.log(table.replace(/\n$/, ''))
+  console.log('-----'.padStart(longest + 4, ' '))
+  console.log()
 }
